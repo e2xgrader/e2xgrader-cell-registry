bea9ffcb3af4dc153e28cdb46f6082c9b6879a29
Tim Daniel Metzler <tim.metzler@h-brs.de>
2025-05-09 14:27:55 +0200

    Lint

--- ui-tests/tests/e2xgrader_cell_registry.spec.ts
+++ ui-tests/tests/e2xgrader_cell_registry.spec.ts
@@ -17,7 +17,9 @@ test('should emit an activation console message', async ({ page }) => {
 
   expect(
     logs.filter(
-      s => s === 'JupyterLab extension @e2xgrader/cell-registry:cell-registry is activated!'
+      s =>
+        s ===
+        'JupyterLab extension @e2xgrader/cell-registry:cell-registry is activated!'
     )
   ).toHaveLength(1);
 });
